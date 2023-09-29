4b1350ec25b9fbffa1187decbc64906e0d29a7ee
Тимофей <tima.antya@mail.ru>
2023-09-29 23:20:19 +0300

    Added views

---
+++ ElectronicJournalCourseProject.WPFApplication/Views/SignInPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElectronicJournalCourseProject.WPFApplication.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        private LoginRepository _loginRepository;
+
+        public SignInPage()
+        {
+            _loginRepository = new LoginRepository();
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Text;
+            bool result = false;
+
+            if (IsTeacherCheckBox.IsChecked == true)
+            {
+                result = _loginRepository.ValidateTeacher(login, password);
+                if (result == true)
+                {
+                    MessageBox.Show("Вы вошли как учитель");
+                }
+            }
+            else
+            {
+                result = _loginRepository.ValidateStudent(login, password);
+                if (result == true)
+                {
+                    MessageBox.Show("Вы вошли как студент");
+                }
+            }
+
+            if (result == false)
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+                return;
+            }
+        }
+    }
+}
