e00d332d92b1be6270936d83e9db981d92753b83
Тимофей <tima.antya@mail.ru>
2023-09-29 23:13:58 +0300

    Edited login repo

--- ElectronicJournalCourseProject.Data/Repositories/LoginRepository.cs
+++ ElectronicJournalCourseProject.Data/Repositories/LoginRepository.cs
@@ -1,33 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElectronicJournalCourseProject.Data.Repositories
+﻿namespace ElectronicJournalCourseProject.Data.Repositories
 {
-    private StudentRepository _studentRepository;
-    private TeacherRepository _teacherRepository;
-
-    public LoginRepository()
+    public class LoginRepository
     {
-        _studentRepository = new StudentRepository();
-        _teacherRepository = new TeacherRepository();
-    }
+        private StudentRepository _studentRepository;
+        private TeacherRepository _teacherRepository;
 
-    public bool ValidateTeacher(string login, string password)
-    {
-        var teachers = _teacherRepository.GetListOfItem();
-        var result = teachers.FirstOrDefault(t => t.Login == login && t.Password == password);
-        if (result == null) return false;
-        return true;
-    }
+        public LoginRepository()
+        {
+            _studentRepository = new StudentRepository();
+            _teacherRepository = new TeacherRepository();
+        }
 
-    public bool ValidateStudent(string login, string password)
-    {
-        var students = _studentRepository.GetListOfItem();
-        var result = students.FirstOrDefault(s => s.Login == login && s.Password == password);
-        if (result == null) return false;
-        return true;
+        public bool ValidateTeacher(string login, string password)
+        {
+            var teachers = _teacherRepository.GetListOfItem();
+            var result = teachers.FirstOrDefault(t => t.Login == login && t.Password == password);
+            if (result == null) return false;
+            return true;
+        }
+
+        public bool ValidateStudent(string login, string password)
+        {
+            var students = _studentRepository.GetListOfItem();
+            var result = students.FirstOrDefault(s => s.Login == login && s.Password == password);
+            if (result == null) return false;
+            return true;
+        }
+
+
     }
 }
