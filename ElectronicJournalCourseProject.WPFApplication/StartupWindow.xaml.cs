05de3736f9e0e292c6bb3e67c8b588b49da64717
Тимофей <tima.antya@mail.ru>
2023-09-29 23:24:34 +0300

    .

---
+++ ElectronicJournalCourseProject.WPFApplication/StartupWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ElectronicJournalCourseProject.Data.DataContext;
+using System.Windows;
+
+namespace ElectronicJournalCourseProject.WPFApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для StartupWindow.xaml
+    /// </summary>
+    public partial class StartupWindow : Window
+    {
+        private ElectronicJournalContextDesignTimeFactory _factory;
+        private string[] _options = { "TIMA\\SQLEXPRESS", "ElectronicJournalDb" };
+
+        public StartupWindow()
+        {
+            _factory = new ElectronicJournalContextDesignTimeFactory();
+            var context = _factory.CreateDbContext(_options);
+            context.Database.EnsureCreated();
+            InitializeComponent();
+        }
+    }
+}
