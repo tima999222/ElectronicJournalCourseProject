6de32c9c1cd79fce089fe4ca6e8025c1c0bf339b
Тимофей <tima.antya@mail.ru>
2023-09-30 16:46:53 +0300

    Added Subject and Mark repos

---
+++ ElectronicJournalCourseProject.Data/Repositories/MarkRepository.cs
@@ -0,0 +1,32 @@
+﻿using ElectronicJournalCourseProject.Data.Entities;
+
+namespace ElectronicJournalCourseProject.Data.Repositories
+{
+    public class MarkRepository : ElectronicJournalBaseRepository<Mark>
+    {
+        public MarkRepository() : base() 
+        { }
+
+        public List<int> GetAllMarksForStudentBySubjectName(int studentId, string subjectName)
+        {
+            var marks = _context.Marks.ToList();
+            var lessons = _context.Lessons.ToList();
+            var loadLists = _context.LoadLists.ToList();
+            var plans = _context.Plans.ToList();
+            var subjects = _context.Subjects.ToList();
+
+            var marksValues = (
+                    from mark in marks
+                    where mark.StudentIdNumber == studentId
+                    join lesson in lessons on mark.LessonId equals lesson.LessonId
+                    join loadList in loadLists on lesson.LoadListId equals loadList.LoadListId
+                    join plan in plans on loadList.PlanId equals plan.PlanId
+                    join subject in subjects on plan.SubjectId equals subject.SubjectId
+                    where subject.SubjectName == subjectName
+                    select mark.MarkValue
+                );
+
+            return marksValues.ToList();
+        }
+    }
+}
---
+++ ElectronicJournalCourseProject.Data/Repositories/SubjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using ElectronicJournalCourseProject.Data.Entities;
+
+namespace ElectronicJournalCourseProject.Data.Repositories
+{
+    public class SubjectRepository : ElectronicJournalBaseRepository<SubjectRepository>
+    {
+        SubjectRepository() : base() 
+        { }
+
+        public Subject GetSubjectById(int id)
+        {
+            var subject = _context.Subjects.FirstOrDefault(subject => subject.SubjectId == id);
+            if (subject == null)
+                throw new Exception();
+
+            return subject;
+        }
+    }
+}
