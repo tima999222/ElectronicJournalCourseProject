d8bdfb04a0675957a9a4d549217035a56662e1c9
Тимофей <tima.antya@mail.ru>
2023-09-30 16:21:59 +0300

    Edited repos

---
+++ ElectronicJournalCourseProject.Data/Repositories/LoadListRepository.cs
@@ -0,0 +1,31 @@
+﻿
+
+using ElectronicJournalCourseProject.Data.Entities;
+
+namespace ElectronicJournalCourseProject.Data.Repositories
+{
+    public class LoadListRepository : ElectronicJournalBaseRepository<LoadList>
+    {
+        public LoadListRepository() : base()
+        { }
+
+        public List<LoadList> GetAllLoadListsForStudentById(int studentId)
+        {
+            var loadList = new List<LoadList>();
+            // groupId = this.GetStudentById(studentId).CurrentGroupId;
+            var student = _context.Students.FirstOrDefault(s => s.StudentIdNumber == studentId);
+
+            if (student == null)
+                throw new Exception();
+
+            var groupId = student.CurrentGroupId;
+            loadList = _context.LoadLists.Where(ll => ll.GroupId == groupId).ToList();
+
+            if (loadList.Count > 0)
+                return loadList;
+            else
+                throw new Exception();
+
+        }
+    }
+}
--- ElectronicJournalCourseProject.Data/Repositories/StudentRepository.cs
+++ ElectronicJournalCourseProject.Data/Repositories/StudentRepository.cs
@@ -17,18 +17,5 @@ namespace ElectronicJournalCourseProject.Data.Repositories
             else
                 throw new Exception();
         }
-
-        public List<LoadList> GetAllLessonsForStudent(int studentId)
-        {
-            var loadList = new List<LoadList>();
-            var groupId = this.GetStudentById(studentId).CurrentGroupId;
-            loadList = _context.LoadLists.Where(ll => ll.GroupId == groupId).ToList();
-
-            if (loadList.Count > 0)
-                return loadList;
-            else 
-                throw new Exception();
-
-        }
     }
 }
